18c91408488ce147896a497eb0cdc8a94be594e2
Flavio Rogerio <solebac@hotmail.com>
2020-11-06 02:23:07 -0300

    First Project MongoDB

---
+++ src/test/java/com/erpro/worshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erpro.worshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
